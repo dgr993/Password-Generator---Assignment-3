99dad76533b24aba2aa4c971f35b99c642e42d27
Wuzup <dgr993@gmail.com>
2020-06-03 17:16:32 -0600

    all 4 variables set up

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,24 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
+  var spchar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  var lowalph = "abcdefghijklmnopqrstuvwxyz"
+  var uppalph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  
+  
+  for (var i = 0; i < 10; i++) {
+    var num = Math.floor(Math.random()*9);
+    console.log(num);
+  } 
+  
   passwordText.value = password;
 
+
+
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
